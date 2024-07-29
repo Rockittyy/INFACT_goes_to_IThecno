d07c7f2ae21d14b4b8278cc6421786972ea789ce
Gzomyx <daivarasendrya1680@gmail.com>
2024-07-29 10:25:55 +0700

    3d upload

--- main/src/pages/WelcomePG.tsx
+++ main/src/pages/WelcomePG.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
+import Spline from '@splinetool/react-spline';
+
 
 interface WelcomePG {
 
@@ -9,26 +11,27 @@ const WelcomePG: FC<WelcomePG> = ({ }) => {
   return (
     <div className='WelcomePG Singgle-Page'>
 
-        <div className="title">
-          <div className="welcome">
-            <h1>WELCOME</h1>
-            <h3>to</h3>
-          </div>
-
-          <h2>INDONESIA</h2>
-          <div className="posHelper1">
-            <h3 className="TxtArchive flex-center"> <span className="text">Archive</span></h3>
-          </div>
-
-
+      <div className="title">
+        <div className="welcome">
+          <h1>WELCOME</h1>
+          <h3>to</h3>
         </div>
 
-        <Link className="clickToContinue1" to={"/About"} >
-            <h4>CLICK TO CONTINUE</h4>
-        </Link>
+        <h2>INDONESIA</h2>
+        <div className="posHelper1">
+          <h3 className="TxtArchive flex-center"> <span className="text">Archive</span></h3>
+        </div>
+
 
       </div>
+
+      <Link className="clickToContinue1" to={"/About"} >
+        <h4>CLICK TO CONTINUE</h4>
+      </Link>
+      <Spline scene="https://prod.spline.design/VnLo8eaQFqSZtkc7/scene.splinecode" />
+    </div>
   )
 }
 
 export default WelcomePG;
+
